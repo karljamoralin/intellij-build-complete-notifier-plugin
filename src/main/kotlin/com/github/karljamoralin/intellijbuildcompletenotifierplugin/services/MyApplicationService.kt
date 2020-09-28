907851bf8f1b04550f1d41b57bb06c531c22360d
GitHub Action <action@github.com>
2020-09-28 10:24:52 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/karljamoralin/intellijbuildcompletenotifierplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.karljamoralin.intellijbuildcompletenotifierplugin.services
+
+import com.github.karljamoralin.intellijbuildcompletenotifierplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
