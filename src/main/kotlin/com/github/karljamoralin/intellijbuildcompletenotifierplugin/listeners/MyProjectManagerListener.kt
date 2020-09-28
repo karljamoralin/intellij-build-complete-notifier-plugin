f593a53d4472336e2c95e444115446aa0507589f
Karl Jamoralin <mail@karlsj.com>
2020-09-28 19:27:19 +0800

    test

--- src/main/kotlin/com/github/karljamoralin/intellijbuildcompletenotifierplugin/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/karljamoralin/intellijbuildcompletenotifierplugin/listeners/MyProjectManagerListener.kt
@@ -3,10 +3,15 @@ package com.github.karljamoralin.intellijbuildcompletenotifierplugin.listeners
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
 import com.github.karljamoralin.intellijbuildcompletenotifierplugin.services.MyProjectService
+import com.intellij.build.BuildProgressListener
+import com.intellij.build.events.BuildEvent
 
-internal class MyProjectManagerListener : ProjectManagerListener {
-
-    override fun projectOpened(project: Project) {
-        project.getService(MyProjectService::class.java)
+internal class MyProjectManagerListener : BuildProgressListener {
+    override fun onEvent(buildId: Any, event: BuildEvent) {
+        println(event.toString())
     }
+
+//    override fun projectOpened(project: Project) {
+//        project.getService(MyProjectService::class.java)
+//    }
 }
